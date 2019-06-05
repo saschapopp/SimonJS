9e7267dd52f78c3fb38b7603f3ef48c5145df67c
= <=>
2019-06-05 17:50:15 +0200

    Service worker

--- docs/app.js
+++ docs/app.js
@@ -1,2 +1,7 @@
 import { simonGame } from './webcomponents/simon.js';
 customElements.define('simon-game', simonGame);
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('serviceWorker.js')
+        .catch(console.error);
+} 
---
+++ docs/serviceWorker.js
@@ -0,0 +1,24 @@
+const swCache = 'simonJS-cache';
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(swCache)
+            .then(cache => cache.addAll([
+                '/simonJS/',
+                'index.html',
+                'app.js',
+                'css/style.css',
+                'webcomponents/simon.css',
+                'webcomponents/simon.html',
+                'webcomponents/simon.js',
+                'icons/icon512.png',
+                'icons/icon16.png',
+            ]))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.open(swCache)
+            .then(cache => cache.match(event.request.url))
+    );
+});
